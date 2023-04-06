57c89171df40ab1c58a97b9daebf78fe5e53f890
camiloflores22 <111587936+camiloflores22@users.noreply.github.com>
2023-04-06 14:26:32 -0400

    Add files via upload

---
+++ MyApp9/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program {
+    static void Main(string[] args) {
+        List<float> notas = new List<float>();
+        float nota;
+
+    Console.WriteLine("Ingrese la nota del estudiante: (-1 para finalizar)");
+    do{
+     nota = float.Parse(Console.ReadLine());
+     if(nota >=0 && nota <= 10) {
+       notas.Add(nota);
+      }
+     }while(nota != -1);
+
+     float promedio=0;
+     foreach (float n in notas) {
+     promedio += n;
+    }
+    promedio /= notas.Count;
+
+    Console.WriteLine("La nota del estudiante es:{0}",promedio);
+     if(promedio >=4) {
+        Console.WriteLine("El Estudiante a aprobado");
+     }else{
+        Console.WriteLine("El Estudiante a Reprobado");
+     }
+     Console.ReadKey();
+
+  }
+ }
